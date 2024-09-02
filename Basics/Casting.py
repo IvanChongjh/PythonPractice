4e0f70d9290423bb65ad43301a5fde470a1be734
IvanCHongjh <ivancjh.six@gmail.com>
2024-09-02 12:27:41 +0800

    added Casting Practice

---
+++ Basics/Casting.py
@@ -0,0 +1,18 @@
+
+# Casting values to int/float.
+x = int(5.2) # this will cast then number 5 into integer.
+print(x)  # notice from output the decimal place will be removed.
+y = float(x)
+print(y) # notice the output once cast x to float the decimal place value is removed.
+
+
+# casting values to string 
+
+z = str("s1")
+print(z)
+
+# since x and y are numbers we should be able to perform basic arithmatics 
+
+o = x + y
+print(o) # output value is 5 + 5.0 = 10
+
