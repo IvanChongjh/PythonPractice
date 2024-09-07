b983b3c8fa62aaffc03441a58638e7630975c9d4
IvanChongjh <ivancjh.six@gmail.com>
2024-09-07 23:12:45 +0800

    added comments for numpy practice

---
+++ Numpy/Array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#  Declaring np to array with 0 - 4 index 
+arr = np.array([1, 2, 3, 4, 5])
+
+
+# printing out entire array 
+print(arr)
+
+#printing out array type
+print(type(arr))
+
+#printing out array index 0
+print(arr[0])
