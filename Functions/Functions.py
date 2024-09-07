0d8d165417a2e677d4bfc3e599454db4f45d1711
IvanChongjh <ivancjh.six@gmail.com>
2024-09-07 23:19:53 +0800

    added comments for functions:

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -1,18 +1,22 @@
 
-# Declaration of functions 
+# Declaration of functions that takes in two parameters x and y respectively  
 def sum(x, y):
+
+    #casting the parameters to float for x and y and store into sum 
     sum  = float(x) + float(y)
     print(sum)
-
+# declaring of functions that takes in two parameters
 def average(x,y):
+    #casting x and y to float and sum it follow by dividing by two
     average = (float(x)+float(y)) / 2
     print(average)
-
+# declaring a function to take in name 
 def modification(name):
     print(name + "\n Murdoch University")
 
+# declaring a function to take in animals 
 def PassingListIntoFunction(animals):
-    for x in animals :
+    for x in animals : # for each loop through animals and print out all details
      print(x)
 
 #Declaration of Numbers for testing
