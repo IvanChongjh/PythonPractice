d4ff659c27f4e29c40224e0a7516ce86072537a6
IvanChongjh <ivancjh.six@gmail.com>
2024-09-10 03:16:29 +0800

    added more examples for class declaration

--- Class/ConstructingClass.py
+++ Class/ConstructingClass.py
@@ -1,10 +1,28 @@
 class Person:
+  # declaring default constructor 
   def __init__(self, name, age):
     self.name = name
     self.age = age
-
+  # declaring class methods
   def myfunc(self):
     print("Hello my name is " + self.name)
 
+
+class Goblin:
+  def __init__(self,health,attacktype):
+    self.health = health 
+    self.attacktype = attacktype
+  def GoblinScreams(self):
+    print("YAWHHHAAAAHHAHAA Tasty")
+  def AttackOnZombie(self, damage):
+    self.health = self.health - damage
+    print("New Zombie Health is " + str(self.health) + "\n Damage Took :" + str(damage) )
+
+
+ # codes starts here 
 p1 = Person("John", 36)
 p1.myfunc() 
+
+g1 = Goblin(150,"range")
+g1.AttackOnZombie(15)
+g1.GoblinScreams()
