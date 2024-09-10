185beba8d7e9b77e50c749043283ff118e2ad896
IvanChongjh <ivancjh.six@gmail.com>
2024-09-10 19:10:53 +0800

    Added 2D array practice and renamed file

--- Numpy/1D.py
+++ Numpy/1D.py
@@ -1,6 +1,9 @@
 import numpy as np
+# purpose of numpy is to provide a faster datastructre with better features for 
+# data analytics compared to tuple
 
 #  Declaring np to array with 0 - 4 index 
+# this is a 1 D array 
 arr = np.array([1, 2, 3, 4, 5])
 
 
@@ -12,3 +15,5 @@ print(type(arr))
 
 #printing out array index 0
 print(arr[0])
+
+
---
+++ Numpy/2D.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+
+# this is a 2D array declared 
+arr = np.array([[1,2,3],[4,5,6]])
+
+print(arr)
